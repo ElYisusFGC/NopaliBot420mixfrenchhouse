d9b1e2b9a707a6f27ccd3f915a87edcdd10596a8
ElYisusFGC <jesusvconde@gmail.com>
2026-04-15 23:52:12 -0600

    Se agregaron los comandos "!x2" y "!sumalos"

--- Nopali-Bot.py
+++ Nopali-Bot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import os
+import re
 #from dotenv import load_dotenv
 
 #load_dotenv(".devcontainer/devcontainer.env")
@@ -17,13 +18,44 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print("el bot420 esta pucheado")
 
-@bot.event
-async def on_message(message: discord.Message):
-    if message.author == bot.user:
+@bot.command(name="420")
+async def fourtwenty(context: discord.ext.commands.Context):
+    print("!420 invocado")
+    await context.channel.send("Tren de pucho")
+
+@bot.command(name="x2")
+async def x2(context: discord.ext.commands.Context, arg: str|None):
+
+    print("!x2 invocado con argumento: " + str(arg))
+
+    if not arg:
+        await context.channel.send("No escribiste nada broo")
+        return
+    elif bool(re.search(r'^\d+$', arg)):
+        res = int(arg) * 2
+    else:
+        await context.channel.send("Eso no es un numero broo")
         return
 
-    if message.content.startswith('!420'):
-        await message.channel.send('Tren de pucho')
+    await context.channel.send("El doble de " + str(arg) + " es " + str(res))
 
+@bot.command(name="sumalos")
+async def suma2(context: discord.ext.commands.Context,*args: str|None):
+
+
+    print("!sumalos invocado con argumentos: " + str(args))
+
+    if not args or not len(args)>=2:
+        await context.channel.send("No puedo sumar menos de 2 numeros bro")
+        return
+    elif bool(re.search(r"^\d+$", args[0])) and bool(re.search(r"^\d+$", args[1])):
+        res = int(args[0]) + int(args[1])
+    if not bool(re.search(r"^\d+$", args[0])):
+        await context.channel.send("El argumento " + str(args[0]) + " no es un numero broo") 
+    if not bool(re.search(r"^\d+$", args[1])):
+        await context.channel.send("El argumento " + str(args[1]) + " no es un numero broo")
+        return
+
+    await context.channel.send("La suma de " + str(args[0]) + " y " + str(args[1]) + " es " + str(res))
 
 bot.run(bot_token)
